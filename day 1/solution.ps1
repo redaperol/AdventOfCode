7858c299d5207cb9f2b1cfde5fcab2e6f1cce41a
antoine.perol <antoine.perol@avanade.com>
2023-12-01 16:16:57 +0100

    fix typo

--- day 1/solution.ps1
+++ day 1/solution.ps1
@@ -2,6 +2,6 @@ $content = Get-Content ./input
 foreach ($line in $content) {
     $Digit = [regex]::Matches("$line","\d")
     [string]$StringNumber = $Digit[0].Value + $Digit[-1].Value
-    $Result+=[int]$StrinNumber
+    $Result+=[int]$StringNumber
 }
 return $Result
