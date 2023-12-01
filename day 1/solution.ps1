743bb6accdfd71fe0639b66e7633c2eef23cfc36
redaperol <96266075+redaperol@users.noreply.github.com>
2023-12-01 16:17:37 +0100

    Update solution.ps1
    removed unnecessary code

--- day 1/solution.ps1
+++ day 1/solution.ps1
@@ -1,16 +1,7 @@
 $content = Get-Content ./input
-$Result = 0
 foreach ($line in $content) {
     $Digit = [regex]::Matches("$line","\d")
-    $StringNumber = ""
-    if ($Digit.Count -eq 1) {
-        $StringNumber = $Digit.value + $Digit.value
-    }
-    elseif ($Digit.Count -gt 1) {
-        $StringNumber = $Digit[0].Value + $Digit[-1].Value
-    }
-    [int]$Number = 0
-    $Number = $StringNumber
-    $Result+=$Number
+    [string]$StringNumber = $Digit[0].Value + $Digit[-1].Value
+    $Result+=[int]$StrinNumber
 }
 return $Result
