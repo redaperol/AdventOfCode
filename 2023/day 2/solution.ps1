cb07da557f5a8992c5fb6449ab7bc38f668e401a
antoine.perol <antoine.perol@avanade.com>
2023-12-02 10:06:42 +0100

    solutions for day 2

---
+++ 2023/day 2/solution.ps1
@@ -0,0 +1,49 @@
+enum color {
+        red = 12
+        green = 13
+        blue = 14
+}
+
+function Assert-ColorValidity {
+    param (
+        [color]
+        $color,
+
+        [string]
+        $Game
+    )
+    $ColorOccurrence = [regex]::Matches($Game,"\d*\s$color").Value
+    foreach ($Occurrence in $ColorOccurrence) {
+        [int]$Number = $Occurrence -replace '[a-z]',''
+        if ($Number -gt $color.value__) {
+            return $false
+        }
+    }
+    return $true
+}
+
+function Assert-GameValidity {
+    param (
+        [string]
+        $Game
+    )
+
+    foreach ($color in [color].GetEnumNames()) {
+        $ColorValidity = Assert-ColorValidity -color $color -Game $Game
+        if (!$ColorValidity) {
+            return $false
+        }
+    }
+    return $true
+}
+
+$GamesArray = Get-Content ./input
+$Result = 0
+foreach ($Game in $GamesArray) {
+    [int]$GameId=[regex]::Match($Game,'(\d*):').Value -replace ":",''
+    $GameValidity = Assert-GameValidity -Game $Game
+    if ($GameValidity) {
+        $Result+=$GameId
+    }
+}
+Return $Result
