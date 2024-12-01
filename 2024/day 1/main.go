c892f99786250d38b95ef77cc25f801afe000c97
antoine.perol <aperol@AVAPC-697851891.>
2024-12-01 20:15:01 +0100

    first solution done

---
+++ 2024/day 1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+func absnum(num int) int {
+	if num >= 0 {
+		return num
+	} else {
+		return num * -1
+	}
+}
+
+func sumArray(arr []int) int {
+	res := 0
+	for i := 0; i < len(arr); i++ {
+		res += arr[i]
+	}
+	return res
+}
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println("pas de bol")
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	reg := regexp.MustCompile(`(\d+)\s{3}(\d+)`)
+	var firstNumberArr []int
+	var seconcdNumberArr []int
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := reg.FindStringSubmatch(line)
+		if len(matches) == 3 {
+			num1, err1 := strconv.Atoi(matches[1])
+			num2, err2 := strconv.Atoi(matches[2])
+			if err1 == nil && err2 == nil {
+				firstNumberArr = append(firstNumberArr, num1)
+				seconcdNumberArr = append(seconcdNumberArr, num2)
+			} else {
+				fmt.Println("Oups convert")
+			}
+		}
+	}
+
+	sort.Ints(firstNumberArr)
+	sort.Ints(seconcdNumberArr)
+	var difArr []int
+	for i := 0; i < len(firstNumberArr); i++ {
+		diffnum := firstNumberArr[i] - seconcdNumberArr[i]
+		difArr = append(difArr, absnum(diffnum))
+	}
+
+	fmt.Println("Done calculatin number diff")
+	fmt.Println("sum is ", sumArray(difArr))
+}
