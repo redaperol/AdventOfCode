b08b8a5e7ed9426763b8a64e9e923a0266465620
antoine.perol <aperol@AVAPC-697851891.>
2024-12-03 23:43:06 +0100

    part 2 done\
    added some refactoring to make the whole thing work

--- 2024/day 3/main.go
+++ 2024/day 3/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"regexp"
@@ -16,44 +15,56 @@ func sumArray(arr []int) int {
 	return res
 }
 
-func GetText(filePath string) []string {
-	var result []string
-	file, err := os.Open("input")
+func fileToStrg(filepath string) (strg string) {
+	buffer, err := os.ReadFile(filepath)
 	if err != nil {
-		fmt.Println("pas de bol")
+		fmt.Println("big oof")
 	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+	strg = string(buffer)
+	return strg
+}
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		result = append(result, line)
+func parserInt(s string, regString string) (firstNumberArr []int, secondNumberArr []int) {
+	regx := regexp.MustCompile(regString)
+
+	match := regx.FindAllStringSubmatch(s, -1)
+
+	for j := 0; j < len(match); j++ {
+		num1, err1 := strconv.Atoi(match[j][1])
+		num2, err2 := strconv.Atoi(match[j][2])
+
+		if err1 == nil && err2 == nil {
+			firstNumberArr = append(firstNumberArr, num1)
+			secondNumberArr = append(secondNumberArr, num2)
+		} else {
+			fmt.Println("Oups convert")
+		}
 	}
-	return result
+	return firstNumberArr, secondNumberArr
+}
+
+func arrProduct(arr1, arr2 []int) (res int) {
+	var resArr []int
+	for i := 0; i < len(arr1); i++ {
+		resArr = append(resArr, arr1[i]*arr2[i])
+	}
+	res = sumArray(resArr)
+	return res
+}
+
+func searchAndDestroyString(strg string, reg string) string {
+	var res string
+	regToUse := regexp.MustCompile(reg)
+	res = regToUse.ReplaceAllString(strg, "")
+	return res
 }
 
 func main() {
-	StringToDo := GetText("testinput")
-	reg := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
-	var firstNumberArr []int
-	var seconcdNumberArr []int
-	for j := 0; j < len(StringToDo); j++ {
-		matches := reg.FindAllStringSubmatch(StringToDo[j], 1000)
-		for i := 0; i < len(matches); i++ {
-			num1, err1 := strconv.Atoi(matches[i][1])
-			num2, err2 := strconv.Atoi(matches[i][2])
-			if err1 == nil && err2 == nil {
-				firstNumberArr = append(firstNumberArr, num1)
-				seconcdNumberArr = append(seconcdNumberArr, num2)
-			} else {
-				fmt.Println("Oups convert")
-			}
-		}
-	}
-	var resArr []int
-	for i := 0; i < len(firstNumberArr); i++ {
-		resArr = append(resArr, firstNumberArr[i]*seconcdNumberArr[i])
-	}
-	result := sumArray(resArr)
-	fmt.Println("part 1 result : ", result)
+	stringToDo := fileToStrg("input")
+	firstArr, secondArr := parserInt(stringToDo, `mul\((\d{1,3}),(\d{1,3})\)`)
+	fmt.Println("part 1 result : ", arrProduct(firstArr, secondArr))
+
+	stringToDoPart2 := searchAndDestroyString(stringToDo, `don't\(\).*?(do\(\)|$|EOF|\n)`)
+	firstArrPart2, secondArrPart2 := parserInt(stringToDoPart2, `mul\((\d{1,3}),(\d{1,3})\)`)
+	fmt.Println("part 2 result : ", arrProduct(firstArrPart2, secondArrPart2))
 }
