d0b59f7143879c716922d190e534638c3741c7c2
aperol <antoineperol@proton.me>
2025-12-01 20:51:10 +0100

    part 1

---
+++ 2025/day1/main.py
@@ -0,0 +1,49 @@
+def add(origin, amount):
+    if origin + amount > 99:
+        result = (origin + amount) - 100
+    else:
+        result = origin + amount
+    return result
+
+
+def sub(origin, amount):
+    if origin - amount < 0:
+        rest = amount - origin
+        result = 99 - rest + 1
+    else:
+        result = origin - amount
+    return result
+
+
+def load_file(path):
+    with open(path) as file:
+        file_input = file.readlines()
+        return file_input
+
+
+def sanitize_input(list):
+    result_list = []
+    for line in list:
+        input = (line[0], int(line[1:]) % 100)
+        result_list.append(input)
+    return result_list
+
+
+def process(tuple, position):
+    direction = tuple[0]
+    amount = tuple[1]
+    if direction == "R":
+        return add(position, amount)
+    elif direction == "L":
+        return sub(position, amount)
+
+
+input = load_file("./input")
+san_input = sanitize_input(input)
+position = 50
+counter = 0
+for i in san_input:
+    position = process(i, position)
+    if position == 0:
+        counter += 1
+print("result part 1:", counter)
